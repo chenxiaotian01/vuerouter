915340267b5145b074e98e45eb4c40d758924eb6
xiaotian <1960243081@qq.com>
2020-06-02 20:47:31 +0800

    'vue'

---
+++ src-02/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div role="tabpanel" class="tab-pane" id="About">
+    <div class="panel panel-default">
+      <div class="panel-body">Basic panel example</div>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {};
+</script>
+
+<style scoped>
+</style>
+
---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="page-header">
+        <h1>路由组件的练习</h1>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <!-- Nav tabs -->
+        <ul class="nav nav-stacked col-md-6" role="tablist">
+          <li role="presentation" class="active">
+              <router-link to="/home" aria-controls="home" role="tab" data-toggle="tab">Home</router-link>
+            <!-- <a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a> -->
+          </li>
+          <li role="presentation">
+              <router-link to="/about" aria-controls="about" role="tab" data-toggle="tab">About</router-link>
+            <!-- <a href="#About" aria-controls="About" role="tab" data-toggle="tab">About</a> -->
+          </li>
+        </ul>
+        <!-- Tab panes -->
+        <div class="tab-content col-md-6">
+            <router-view></router-view>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {};
+</script>
+
+<style scoped>
+</style>
